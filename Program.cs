513d8ba8321a2b44029de47a0a331b3bdbd73fe6
xaloftal <dianaacdinis@gmail.com>
2024-09-19 10:10:38 +0100

    kjahsd

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using mqtt_broker.model;
+using mqtt_broker.Services;
+
+
+Console.WriteLine("Starting MQTT Data Aggregator...");
+
+// Create an instance of the MqttDataAggregator class
+var aggregator = new Aggregator();
+
+Thread threadAggregator = new Thread(async () =>
+{
+    await aggregator.StartAsync();
+});
+
+threadAggregator.Start();
+
+Console.WriteLine("MQTT Data Aggregator started. Press any key to exit...");
+
+// Keep the program running until a key is pressed
+Console.ReadKey();
+
+Console.WriteLine("Shutting down...");
---
+++ model/Recipe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mqtt_broker.model
+{
+    /// <summary>
+    /// Class for the recipies, to use for processing data
+    /// </summary>
+    public class Recipe
+    {
+        /// <summary>
+        /// name of the recipe
+        /// </summary>
+        public string Name { get; set; }
+       
+        /// <summary>
+        /// Recipe description, explains what it does
+        /// </summary>
+        public string Description { get; set; }
+          
+        /// <summary>
+        /// The strategy (ex: sum, average...)
+        /// </summary>
+        public string Strategy { get; set; }
+       
+        /// <summary>
+        /// How long data is collected before processing it
+        /// </summary>
+        public int WindowSize { get; set; }
+
+        /// <summary>
+        /// What topics this recipe is applied to
+        /// </summary>
+        public List<string> Topics { get; set; }
+
+    }
+}
